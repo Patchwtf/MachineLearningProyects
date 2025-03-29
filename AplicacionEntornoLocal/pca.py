dd7927750451cbc943356a09a3de4072fafa4013
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2025-03-28 22:11:11 -0600

    Inicio de PCA - Importacion de librerias

---
+++ AplicacionEntornoLocal/pca.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA, IncrementalPCA
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
